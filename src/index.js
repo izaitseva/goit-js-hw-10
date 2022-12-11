637b23d2c24262b444e34c8cfaa1f924ed2576b6
Iryna Zaitseva <zaitseva02@gmail.com>
2022-12-11 12:22:29 +0200

    css edits

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ let countriesInfo = document.querySelector('.country-info')
 let countrieslist = document.querySelector('.country-list');
 let inputRef = document.getElementById('search-box');
 
+console.log('loh');
+
 inputRef.addEventListener('input', debounce(() => {
     onInput();
 }, DEBOUNCE_DELAY));
@@ -26,17 +28,17 @@ function onInput() {
                 Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
             } else if (data.length >= 2 && data.length <= 10) {
                 const markup = data.map(item => `
-                <li class="item">
-                    <img src="${item.flags.svg}" alt="flag_image">
-                    <p class="name">Country: ${item.name.official}</p>
+                <li class="item item_2">
+                    <img class="img_2" src="${item.flags.svg}" alt="flag_image">
+                    <p class="name_2">${item.name.official}</p>
                 </li>
                 `).join('')
                 countrieslist.innerHTML = markup
             } else {
                 const markup = data.map(item => `
                         <li class="item">
-                            <img src="${item.flags.svg}" alt="flag_image">
-                            <p class="name">Country: ${item.name.official}</p>
+                            <img class="img" src="${item.flags.svg}" alt="flag_image">
+                            <p class="name">${item.name.official}</p>
                             <p class="capital">Capital: ${item.capital}</p>
                             <p class="population">Population: ${item.population}</p>
                             <p class="languages">Languages: ${item.languages ? Object.values(item.languages).toString() : ''}</p>
