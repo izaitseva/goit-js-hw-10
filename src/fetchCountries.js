6df9df786e2b8ffcf4c9614257e244da60ec7079
Iryna Zaitseva <zaitseva02@gmail.com>
2022-12-12 17:15:47 +0200

    hw edits

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,13 @@
 export function fetchCountries(name) {
     return fetch(`https://restcountries.com/v3.1/name/${name}`)
         .then(response => response.json())
+        .then(data => data.map(element => {
+            return {
+                name: element.name.official,
+                capital: element.capital,
+                population: element.population,
+                languages: element.languages,
+                flag: element.flags.svg,
+            }
+        }))
 }
--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ let countriesInfo = document.querySelector('.country-info')
 let countrieslist = document.querySelector('.country-list');
 let inputRef = document.getElementById('search-box');
 
-console.log('loh');
-
 inputRef.addEventListener('input', debounce(() => {
     onInput();
 }, DEBOUNCE_DELAY));
@@ -29,16 +27,16 @@ function onInput() {
             } else if (data.length >= 2 && data.length <= 10) {
                 const markup = data.map(item => `
                 <li class="item item_2">
-                    <img class="img_2" src="${item.flags.svg}" alt="flag_image">
-                    <p class="name_2">${item.name.official}</p>
+                    <img class="img_2" src="${item.flag}" alt="flag_image">
+                    <p class="name_2">${item.name}</p>
                 </li>
                 `).join('')
                 countrieslist.innerHTML = markup
             } else {
                 const markup = data.map(item => `
                         <li class="item">
-                            <img class="img" src="${item.flags.svg}" alt="flag_image">
-                            <p class="name">${item.name.official}</p>
+                            <img class="img" src="${item.flag}" alt="flag_image">
+                            <p class="name">${item.name}</p>
                             <p class="capital"><b>Capital:</b> ${item.capital}</p>
                             <p class="population"><b>Population:</b> ${item.population}</p>
                             <p class="languages"><b>Languages:</b> ${item.languages ? Object.values(item.languages).toString() : ''}</p>
