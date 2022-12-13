35dd34efbb962610788e77c44774dffbb6c25686
Iryna Zaitseva <zaitseva02@gmail.com>
2022-12-13 22:48:51 +0200

    url edit

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,5 @@
 export function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}`)
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`)
         .then(response => response.json())
         .then(data => data.map(element => {
             return {
